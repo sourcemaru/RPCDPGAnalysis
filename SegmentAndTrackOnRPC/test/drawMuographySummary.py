64e2efe0b270e34390a28815ee505c8d5115dd1b
Junghwan John Goh <jhgoh@cern.ch>
2016-06-14 04:15:21 +0200

    RPc color table

--- SegmentAndTrackOnRPC/test/drawMuographySummary.py
+++ SegmentAndTrackOnRPC/test/drawMuographySummary.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
+
+run = 274442
+padW = 500
+
 from ROOT import *
 from array import array
 gStyle.SetOptStat(0)
-gROOT.ProcessLine("""struct customDivide { customDivide(const TH2D* h1, const TH2D* h2, TH2D* heff) {
-  for ( int i=0, n=(h1->GetNbinsX()+1)*(h1->GetNbinsY()); i<n; ++i ) {
-    const double den = h1->GetBinContent(i);
-    const double num = h2->GetBinContent(i);
-    heff->SetBinContent(i, den == 0 ? -1: num/den);
-  }
-}};""")
-
-run=274442
 
 f = TFile("hist.root")
 
@@ -38,9 +33,17 @@ hEndcapN = [
 hDens, hEffs = [], []
 maxMeanZ, maxZ = 0.0, 0.0
 
-cBDen = TCanvas("cBDen", "Barrel statistics", 1200, 800)
+gROOT.ProcessLine("""struct customDivide { customDivide(const TH2D* h1, const TH2D* h2, TH2D* heff) {
+  for ( int i=0, n=(h1->GetNbinsX()+1)*(h1->GetNbinsY()); i<n; ++i ) {
+    const double den = h1->GetBinContent(i);
+    const double num = h2->GetBinContent(i);
+    heff->SetBinContent(i, den == 0 ? -1 : num/den);
+  }
+}};""")
+
+cBDen = TCanvas("cBDen", "Barrel statistics", 3*padW, 2*padW)
 cBDen.Divide(3,2)
-cBEff = TCanvas("cBEff", "Barrel efficiency", 1200, 800)
+cBEff = TCanvas("cBEff", "Barrel efficiency", 3*padW, 2*padW)
 cBEff.Divide(3,2)
 for i, (hDenName, hNumName) in enumerate(hBarrel):
     hDen = f.Get("rpcPoint/Run%d/%s" % (run, hDenName))
@@ -62,9 +65,9 @@ for i, (hDenName, hNumName) in enumerate(hBarrel):
     cBEff.cd(i+1)
     hEff.Draw("COLZ")
 
-cEPDen = TCanvas("cEPDen", "Endcap+ statistics", 800, 800)
+cEPDen = TCanvas("cEPDen", "Endcap+ statistics", 2*padW, 2*padW)
 cEPDen.Divide(2,2)
-cEPEff = TCanvas("cEPEff", "Endcap+ efficiency", 800, 800)
+cEPEff = TCanvas("cEPEff", "Endcap+ efficiency", 2*padW, 2*padW)
 cEPEff.Divide(2,2)
 for i, (hDenName, hNumName) in enumerate(hEndcapP):
     hDen = f.Get("rpcPoint/Run%d/%s" % (run, hDenName))
@@ -76,6 +79,8 @@ for i, (hDenName, hNumName) in enumerate(hEndcapP):
 
     cEPDen.cd(i+1)
     hDen.Draw("COLZ")
+    hDen.GetXaxis().SetRangeUser(-800, 800)
+    hDen.GetYaxis().SetRangeUser(-800, 800)
 
     sumZ = 0
     nBins = (hDen.GetNbinsX()+2)*(hDen.GetNbinsY()+2)
@@ -85,10 +90,12 @@ for i, (hDenName, hNumName) in enumerate(hEndcapP):
 
     cEPEff.cd(i+1)
     hEff.Draw("COLZ")
+    hEff.GetXaxis().SetRangeUser(-800, 800)
+    hEff.GetYaxis().SetRangeUser(-800, 800)
 
-cENDen = TCanvas("cENDen", "Endcap- statistics", 800, 800)
+cENDen = TCanvas("cENDen", "Endcap- statistics", 2*padW, 2*padW)
 cENDen.Divide(2,2)
-cENEff = TCanvas("cENEff", "Endcap- efficiency", 800, 800)
+cENEff = TCanvas("cENEff", "Endcap- efficiency", 2*padW, 2*padW)
 cENEff.Divide(2,2)
 for i, (hDenName, hNumName) in enumerate(hEndcapN):
     hDen = f.Get("rpcPoint/Run%d/%s" % (run, hDenName))
@@ -100,6 +107,8 @@ for i, (hDenName, hNumName) in enumerate(hEndcapN):
 
     cENDen.cd(i+1)
     hDen.Draw("COLZ")
+    hDen.GetXaxis().SetRangeUser(-800, 800)
+    hDen.GetYaxis().SetRangeUser(-800, 800)
 
     sumZ = 0
     nBins = (hDen.GetNbinsX()+2)*(hDen.GetNbinsY()+2)
@@ -109,16 +118,32 @@ for i, (hDenName, hNumName) in enumerate(hEndcapN):
 
     cENEff.cd(i+1)
     hEff.Draw("COLZ")
+    hEff.GetXaxis().SetRangeUser(-800, 800)
+    hEff.GetYaxis().SetRangeUser(-800, 800)
 
-
+gStyle.SetPalette(kBird)
 for h in hDens:
     h.SetMaximum(maxMeanZ*1.2)
-levels = array('d', [0.01*i for i in range(0, 101)])
-for h in hEffs:
-    h.SetMaximum(1.0)
-    h.SetMinimum(-1e-3)
-    h.SetContour(len(levels)-1, levels)
-
+    h.UseCurrentStyle()
 for c in (cBDen, cEPDen, cENDen):
-    c.Modified()
-    c.Update()
+    for i in range(10):
+        p = c.GetPad(i)
+        if p == None: break
+        p.Modified()
+        p.Update()
+
+rpcPalette = array('i', [kBlack,632,632,632,632,632,632,632,807,400,416,kBlue])
+levels = array('d', [0.1*i+1e-9 for i in range(-1, 12)])
+gStyle.SetPalette(len(rpcPalette), rpcPalette);
+for h in hEffs:
+    h.SetMaximum(1.1)
+    h.SetMinimum(-0.1)
+    h.SetContour(len(levels)-1, levels)
+    h.UseCurrentStyle()
+for c in (cBEff, cEPEff, cENEff):
+    for i in range(10):
+        p = c.GetPad(i)
+        if p == None: break
+        p.Modified()
+        p.Update()
+
