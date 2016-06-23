a621a230bb86ae4271a4ac560bc2b5f1b97f114f
Junghwan John Goh <jhgoh@cern.ch>
2016-06-23 16:52:17 +0200

    cmsDriver options

--- ChamberDegradation/test/run.sh
+++ ChamberDegradation/test/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cmsDriver.py step2 --filein /store/relval/CMSSW_8_0_10/RelValSingleMuPt100_UP15/GEN-SIM-DIGI-RAW-HLTDEBUG/80X_mcRun2_asymptotic_v14-v1/00000/42AC15DA-C528-E611-9790-0CC47A78A340.root --fileout step2.root --eventcontent RECOSIM --mc --conditions auto:mc --step RAW2DIGI,L1Reco,RECO,EI,DQM --era Run2_25ns  --python step2_cfg.py -n -1 --no_exec --customise RPCDPGAnalysis/ChamberDegradation/customise_cff.customise_DropMuonHits
+cmsDriver.py step2 --conditions auto:run2_mc --era Run2_25ns --mc --magField 38T_PostLS1 \
+  --step RAW2DIGI,L1Reco,RECO,EI,DQM -n -1 \
+  --fileout step2.root --eventcontent RECOSIM \
+  --filein /store/relval/CMSSW_8_0_10/RelValSingleMuPt100_UP15/GEN-SIM-DIGI-RAW-HLTDEBUG/80X_mcRun2_asymptotic_v14-v1/00000/42AC15DA-C528-E611-9790-0CC47A78A340.root \
+  --customise RPCDPGAnalysis/ChamberDegradation/customise_cff.customise_DropMuonHits \
+  --no_exec --python step2_cfg.py 
 
 cmsRun step2_cfg.py
