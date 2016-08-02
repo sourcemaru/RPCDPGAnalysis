1721b5012334cea2cf92a83e4753c2ab2ef699ba
Junghwan John Goh <jhgoh@cern.ch>
2016-08-02 20:34:20 +0200

    cut value tuning

--- SegmentAndTrackOnRPC/python/rpcPointFromTagProbeProducer_cff.py
+++ SegmentAndTrackOnRPC/python/rpcPointFromTagProbeProducer_cff.py
@@ -11,20 +11,20 @@ rpcPointFromTagProbe = cms.EDProducer("RPCPointFromTagProbeProducer",
     muons = cms.InputTag("muons"),
     minMuonPt = cms.double(25),
     maxMuonAbsEta = cms.double(2.4),
-    maxMuonRelIso = cms.double(0.15),
+    maxMuonRelIso = cms.double(0.25), # Loose isolation ~98% eff. (tight=0.15)
     minTrackPt = cms.double(20),
-    maxTrackAbsEta = cms.double(1.9),
+    maxTrackAbsEta = cms.double(2.1),
     minMass = cms.double(70),
     maxMass = cms.double(110),
     triggerObjects = cms.InputTag("hltTriggerSummaryAOD"),
     triggerResults = cms.InputTag("TriggerResults::HLT"),
-    triggerPaths = cms.vstring("HLT_IsoMu22", "HLT_IsoMu22_eta2p1", "HLT_IsoTkMu22", "HLT_Mu50"),
+    triggerPaths = cms.vstring("HLT_IsoMu22", "HLT_IsoMu22_eta2p1", "HLT_IsoTkMu22", "HLT_Mu50", "HLT_TkMu50"),
 )
 
 rpcPointFromTrackerMuons = cms.EDProducer("RPCPointFromTrackerMuonProducer",
     #vertex = cms.InputTag("offlinePrimaryVertices"),
     muons = cms.InputTag("muons"),
-    minMuonPt = cms.double(25),
-    maxMuonAbsEta = cms.double(2.4),
+    minMuonPt = cms.double(20),
+    maxMuonAbsEta = cms.double(2.1),
 )
 
