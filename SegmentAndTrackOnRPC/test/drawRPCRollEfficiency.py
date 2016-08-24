3c31e54fc7c8b4b11963eb594ac0624d3c321e76
Junghwan John Goh <jhgoh@cern.ch>
2016-08-24 03:20:15 +0200

    print out roll eff list, better styling

--- SegmentAndTrackOnRPC/test/drawRPCRollEfficiency.py
+++ SegmentAndTrackOnRPC/test/drawRPCRollEfficiency.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 run = 0#274442
-padW = 500
 #mode = "tmPoint"
 mode = "tpPoint"
+#mode = "tvPoint"
 #mode = "rpcPoint"
 
 from ROOT import *
@@ -13,8 +13,16 @@ gROOT.ProcessLine(".L %s/src/SUSYBSMAnalysis/HSCP/test/ICHEP_Analysis/tdrstyle.C
 setTDRStyle()
 gStyle.SetOptStat(0)
 
-f = TFile("20160806/SingleMuon_Run2016BCD.root")
+gStyle.SetPadTopMargin(0.07)
+gStyle.SetPadLeftMargin(0.12)
+gStyle.SetPadRightMargin(0.048)
+gStyle.SetPadBottomMargin(0.12)
+gStyle.SetTitleSize(0.06, "X");
+gStyle.SetTitleSize(0.06, "Y");
 
+f = TFile("SingleMuon_Run2016BCDE.root")
+
+rollNames = [[], [], []]
 nExps = [[], [], []]
 nRPCs = [[], [], []]
 
@@ -35,6 +43,7 @@ for s in [x.GetName() for x in f.Get("%s/Run%06d/Barrel" % (mode, run)).GetListO
                 n = [h.GetBinContent(i+1) for i in range(h.GetNbinsX())]
                 if s.startswith("RollExp_"): nExps[0].extend(n)
                 if s.startswith("RollRPC_"): nRPCs[0].extend(n)
+                if s.startswith("RollExp_"): rollNames[0].extend([h.GetXaxis().GetBinLabel(i+1) for i in range(h.GetNbinsX())])
 
 for s in [x.GetName() for x in f.Get("%s/Run%06d/Endcap+" % (mode, run)).GetListOfKeys()]:
     if not s.startswith("Disk_"): continue
@@ -53,6 +62,7 @@ for s in [x.GetName() for x in f.Get("%s/Run%06d/Endcap+" % (mode, run)).GetList
                 n = [h.GetBinContent(i+1) for i in range(h.GetNbinsX())]
                 if s.startswith("RollExp_"): nExps[1].extend(n)
                 if s.startswith("RollRPC_"): nRPCs[1].extend(n)
+                if s.startswith("RollExp_"): rollNames[1].extend([h.GetXaxis().GetBinLabel(i+1) for i in range(h.GetNbinsX())])
 
 for s in [x.GetName() for x in f.Get("%s/Run%06d/Endcap-" % (mode, run)).GetListOfKeys()]:
     if not s.startswith("Disk_"): continue
@@ -71,48 +81,74 @@ for s in [x.GetName() for x in f.Get("%s/Run%06d/Endcap-" % (mode, run)).GetList
                 n = [h.GetBinContent(i+1) for i in range(h.GetNbinsX())]
                 if s.startswith("RollExp_"): nExps[2].extend(n)
                 if s.startswith("RollRPC_"): nRPCs[2].extend(n)
+                if s.startswith("RollExp_"): rollNames[2].extend([h.GetXaxis().GetBinLabel(i+1) for i in range(h.GetNbinsX())])
 
+binW, xmin, xmax = 0.5, 70.5, 100
+nbin = int((xmax-xmin)/binW)
 objs = []
 hEffs = [
-    TH1F("hEffBarrel", "Barrel;Efficiency [%];RPC rolls per 1%", 101, -0.5, 100.5),
-    TH1F("hEffEndcap+", "Endcap+;Efficiency [%];RPC rolls per 1%", 101, -0.5, 100.5),
-    TH1F("hEffEndcap-", "Endcap-;Efficiency [%];RPC rolls per 1%", 101, -0.5, 100.5),
+    TH1F("hEffBarrel", "Barrel;Efficiency [%];Number of Rolls", nbin+1, xmin, xmax+binW),
+    TH1F("hEffEndcapP", "Endcap+;Efficiency [%];Number of Rolls", nbin+1, xmin, xmax+binW),
+    TH1F("hEffEndcapN", "Endcap-;Efficiency [%];Number of Rolls", nbin+1, xmin, xmax+binW),
 ]
 canvs = [
-    TCanvas("cBarrel", "cBarrel", 500, 500),
-    TCanvas("cEndcap+", "cBarrel", 500, 500),
-    TCanvas("cEndcap-", "cBarrel", 500, 500),
+    TCanvas("cBarrel", "cBarrel", 800, 585),
+    TCanvas("cEndcapP", "cEndcapP", 800, 585),
+    TCanvas("cEndcapN", "cEndcapN", 800, 585),
 ]
 for i in range(3):
-    effs = [100*nRPC/nExp for nExp, nRPC in zip(nExps[i], nRPCs[i]) if nExp > 100]
-    effs.sort(reverse=True)
+    effs = [(name, 100*nRPC/nExp) for name, nExp, nRPC in zip(rollNames[i], nExps[i], nRPCs[i]) if nExp > 100]
+    effs.sort(reverse=True, key=lambda x : x[1])
 
-    for eff in effs: hEffs[i].Fill(eff)
+    hEffs[i].GetYaxis().SetNdivisions(505)
+    hEffs[i].GetYaxis().SetTitleOffset(1.0)
+
+    for eff in effs: hEffs[i].Fill(eff[1])
 
     peak = hEffs[i].GetBinCenter(hEffs[i].GetMaximumBin())
-    if effs[-1] != 0.0: effsNoZero = effs[:]
-    else: effsNoZero = effs[:effs.index(0.0)]
+    effsNoZero = [x for x in effs if x[1] != 0.0]
     median = 0
-    if len(effsNoZero)%2 == 1: median = effsNoZero[len(effsNoZero)/2]
+    if len(effsNoZero)%2 == 1: median = effsNoZero[len(effsNoZero)/2][1]
     else: median = (effsNoZero[len(effsNoZero)/2]+effsNoZero[len(effsNoZero)/2+1])/2
 
+    header = TLatex(gStyle.GetPadLeftMargin(), 1-gStyle.GetPadTopMargin()+0.01,
+                    "RPC Overall Efficiency - %s" % hEffs[i].GetTitle())
+    header.SetNDC()
+    header.SetTextAlign(11)
+    header.SetTextFont(42)
+
     stats = []
-    stats.append(hEffs[i].GetTitle())
-    stats.append("Average (include zero) = %.1f" % (sum(effs)/len(effs)))
-    stats.append("Average (without zero) = %.1f" % (sum(effsNoZero)/len(effsNoZero)))
+    stats.append("Average (include zero) = %.1f" % (sum([x[1] for x in effs])/len(effs)))
+    stats.append("Average (without zero) = %.1f" % (sum([x[1] for x in effsNoZero])/len(effsNoZero)))
     stats.append("Median  (without zero) = %.1f" % median)
     stats.append("Peak at                = %.1f" % hEffs[i].GetBinCenter(hEffs[i].GetMaximumBin()))
 
-    statPanel = TPaveText(gStyle.GetPadLeftMargin()+0.04, 1-gStyle.GetPadTopMargin()-6*0.06,
-                          0.7, 1-gStyle.GetPadTopMargin()-0.06, "NDC")
+    statPanel = TPaveText(1-gStyle.GetPadRightMargin()-0.5, 1-gStyle.GetPadTopMargin()-len(stats)*0.06,
+                          1-gStyle.GetPadRightMargin()-0.1, 1-gStyle.GetPadTopMargin()-0.06, "NDC")
+    statPanel.SetFillStyle(0)
     statPanel.SetTextAlign(11)
+    statPanel.SetBorderSize(0)
+    statPanel.SetTextFont(102)
     for s in stats: statPanel.AddText(s)
 
     canvs[i].cd()
     hEffs[i].Draw()
     statPanel.Draw()
-    statPanel.SetBorderSize(0)
+    header.Draw()
+
+    lumi = TLatex().DrawLatexNDC(1-gStyle.GetPadRightMargin(), 1-gStyle.GetPadTopMargin()+0.01,
+                                 "%.1f fb^{-1} (13 TeV)" % 16.094782029242)
+    lumi.SetTextAlign(31)
+    lumi.SetTextFont(42)
+
     fixOverlay()
 
-    objs.append(statPanel)
+    objs.extend([header, lumi, statPanel])
 
+    for l in effs:
+        print l[0], l[1]
+
+for c in canvs:
+    c.Update()
+    c.Print(c.GetName()+".png")
+    c.Print(c.GetName()+".C")
