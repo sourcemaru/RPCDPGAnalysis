b8b14e69296ddcf18eed93e1f88fe0bcd684ed49
Junghwan John Goh <jhgoh@cern.ch>
2017-02-23 03:30:40 +0100

    use rereco dataset, reduce job splitting

--- SegmentAndTrackOnRPC/test/crabConfig.py
+++ SegmentAndTrackOnRPC/test/crabConfig.py
@@ -22,31 +22,25 @@ config.Data.splitting = 'LumiBased'
 
 ## Something that can be changed frequently
 import os
-if 'ERA' not in os.environ: era = "Run2016B"
-else: era = os.environ['ERA']
-if 'DATASET' not in os.environ: dataset = "SingleMuon"
-else: dataset = os.environ['DATASET']
+config.Data.inputDataset = "/SingleMuon/Run2016B-23Sep2016-v3/AOD"
+if 'DATASET' in os.environ:
+    config.Data.inputDataset = os.environ['DATASET']
 
-if era[:-1] == "Run2016" and era[-1] in "BCDE": datasetVer = 'v2'
-else: datasetVer = 'v1'
+pd, sd = config.Data.inputDataset.split('/')[1:3]
 
-if dataset == 'RPCMonitor':
-    config.Data.unitsPerJob = 10
+if pd == 'RPCMonitor':
+    config.Data.unitsPerJob = 50
     config.JobType.psetName    = 'analyzeRPCwithSegments_cfg.py'
-    config.Data.inputDataset = "/%s/%s-%s/RAW" % (dataset, era, datasetVer)
 else:
-    config.Data.unitsPerJob = 40
+    config.Data.unitsPerJob = 200
     config.JobType.psetName    = 'analyzeRPCwithTnP_cfg.py'
-    config.Data.inputDataset = "/%s/%s-PromptReco-%s/AOD" % (dataset, era, datasetVer)
 
 username = os.environ['USER']
 
 from datetime import datetime as dt
 submitdate = dt.now().strftime('%Y%m%d')
 
-#config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-279116_13TeV_PromptReco_Collisions16_JSON_NoL1T_MuonPhys.txt'
-config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-277148_13TeV_PromptReco_Collisions16_JSON_MuonPhys.txt'
-#config.Data.lumiMask = 'notFinishedLumis.json'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON_MuonPhys.txt'
 config.Data.outLFNDirBase = '/store/user/%s/RPCChamberEfficiency/%s_1' % (username, submitdate)
-config.General.requestName = "RPCEfficiency_%s_%s" % (dataset, era)
+config.General.requestName = "RPCEfficiency_%s_%s" % (pd, sd)
 
