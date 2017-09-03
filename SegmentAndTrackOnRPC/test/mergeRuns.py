ab448842774dad987d6b1ab112efcdb3d55510ed
Junghwan John Goh <jhgoh@cern.ch>
2017-09-04 01:04:06 +0200

    remove hardcoded file name pattern

--- SegmentAndTrackOnRPC/test/mergeRuns.py
+++ SegmentAndTrackOnRPC/test/mergeRuns.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python
 
 from ROOT import *
-import os
+import sys, os
 
-files_SingleMuon = [TFile("SingleMuon_Run2016%s.root" % i) for i in "BCDE"]
-runs_SingleMuon = []
-for d in [f.Get("rpcExt") for f in files_SingleMuon]: runs_SingleMuon.extend([x.GetName() for x in d.GetListOfKeys()])
-
-files_RPCMonitor = [TFile("RPCMonitor_Run2016%s.root" % i) for i in "BCDE"]
-runs_RPCMonitor = []
-for d in [f.Get("rpcPoint") for f in files_RPCMonitor]: runs_RPCMonitor.extend([x.GetName() for x in d.GetListOfKeys()])
-
-runs = list(set(runs_SingleMuon).intersection(set(runs_RPCMonitor)))
-runs.sort()
+#files = [TFile("SingleMuon_Run2016%s.root" % i) for i in "BCDE"]
+files  = [TFile(x) for x in sys.argv[1:] if x.endswith('.root')]
+runs = []
+for d in [f.Get("rpcExt") for f in files]: runs.extend([x.GetName() for x in d.GetListOfKeys()])
 
 def collectNames(d):
     names = []
     for name in [x.GetName() for x in d.GetListOfKeys()]:
+        if 'XY' in name or 'ZPhi' in name: continue
         obj = d.Get(name)
         if obj.IsA().InheritsFrom("TDirectory"): names.extend(collectNames(obj))
-        else: names.append("/".join(d.GetPath().split('/')[3:]+[name]))
+        else: names.append("/".join(d.GetPath().split('.root:',1)[-1].split('/')[3:]+[name]))
     return names
-names = collectNames(files_SingleMuon[0].Get("rpcExt/%s" % runs[0]))
+names = collectNames(files[0].Get("rpcExt/%s" % runs[0]))
 runs = []
 
 def mkdirs(d, path):
@@ -31,34 +26,32 @@ def mkdirs(d, path):
         else: d = d.GetDirectory(pname)
     return d
 
-for prefix in ["SingleMuon", "RPCMonitor"]:
-    fout = TFile("%s.root" % prefix, "RECREATE")
+fout = TFile("merged.root", "RECREATE")
 
-    houts = []
-    for suffix in "BCDE":
-        fin = TFile("%s_Run2016%s.root" % (prefix, suffix))
-        din = fin.Get("rpcExt")
-        if din == None: din = fin.Get("rpcPoint")
-        if din == None: continue
+houts = []
+for fin in files:
+    din = fin.Get("rpcExt")
+    if din == None: din = fin.Get("rpcPoint")
+    if din == None: continue
 
-        nRuns = max(len(runs), len(din.GetListOfKeys()))
-        for ii, run in enumerate([x.GetName() for x in din.GetListOfKeys()]):
-            if len(runs) > 0 and run not in runs: continue
-            print "%s %s (%d/%d)" % (fin.GetName(), run, ii+1, nRuns)
-            for name in names:
-                hin = din.Get(run+"/"+name)
-                if hin == None: continue
+    nRuns = max(len(runs), len(din.GetListOfKeys()))
+    for ii, run in enumerate([x.GetName() for x in din.GetListOfKeys()]):
+        if len(runs) > 0 and run not in runs: continue
+        print "%s %s (%d/%d)" % (fin.GetName(), run, ii+1, nRuns)
+        for name in names:
+            hin = din.Get(run+"/"+name)
+            if hin == None: continue
 
-                dout = mkdirs(fout, os.path.dirname(name))
-                dout.cd()
-                hout = dout.Get(os.path.basename(name))
-                if hout == None:
-                    hout = hin.Clone()
-                    houts.append([dout, hout])
-                else: hout.Add(hin)
+            dout = mkdirs(fout, os.path.dirname(name))
+            dout.cd()
+            hout = dout.Get(os.path.basename(name))
+            if hout == None:
+                hout = hin.Clone()
+                houts.append([dout, hout])
+            else: hout.Add(hin)
 
-    for dout, hout in houts:
-        dout.cd()
-        hout.Write()
-    fout.Close()
-    fout = None
+for dout, hout in houts:
+    dout.cd()
+    hout.Write()
+fout.Close()
+fout = None
