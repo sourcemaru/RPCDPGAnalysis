e1af77940280e4f5b9f84b0c07954717ca290e12
Junghwan John Goh <jhgoh@cern.ch>
2018-08-14 11:20:46 +0200

    modify project_efficiency to be a function, to be called from a ipython notebook

--- SegmentAndTrackOnRPC/test/project_efficiency.py
+++ SegmentAndTrackOnRPC/test/project_efficiency.py
@@ -1,60 +1,64 @@
 #!/usr/bin/env python
 
-from ROOT import *
 import os, sys
-gStyle.SetOptStat(0)
+if "CMSSW_BASE" in os.environ:
+    sys.path.append("%s/src/RPCDPGAnalysis/SegmentAndTrackOnRPC/python" % os.environ["CMSSW_BASE"])
 
-sys.path.append("%s/src/RPCDPGAnalysis/SegmentAndTrackOnRPC/python" % os.environ["CMSSW_BASE"])
-from ProjectTHnSparse import *
+def project(fName, commonSel):
+    from ROOT import gStyle, TFile
+    gStyle.SetOptStat(0)
+    from ProjectTHnSparse import THnSparseSelector
 
-print "@@ Opening root file..."
-f = TFile(sys.argv[1])
-print "@@ Loading RPC TnP histogram..."
-hInfo = f.Get("rpcExt/hInfo")
-print "@@ Initializing THnSparseSelector..."
-hSel = THnSparseSelector(hInfo)
+    print "@@ Opening root file..."
+    f = TFile(fName)
+    print "@@ Loading RPC TnP histogram..."
+    hInfo = f.Get("rpcExt/hInfo")
+    print "@@ Initializing THnSparseSelector..."
+    hSel = THnSparseSelector(hInfo)
 
-if not os.path.exists("hists"): os.mkdir("hists")
+    print "@@ Extracting runs..."
+    hRuns = hSel.Project1D("run", commonSel)
+    #runs = [i+2 for i in range(hRuns.GetNbinsX()) if hRuns.GetBinContent(i+2) != 0]
+    runs = [hRuns.GetXaxis().GetBinLowEdge(i+1) for i in range(hRuns.GetNbinsX()) if hRuns.GetBinContent(i+1) != 0]
+    print "@@ Extracting roll names..."
+    hRolls = hSel.Project1D("rollName", {}, copyAxisLabel=True)
+    axisRolls = hRolls.GetXaxis()
+    rollNames = [axisRolls.GetBinLabel(i) for i in range(hRolls.GetNbinsX()+2) if axisRolls.GetBinLabel(i) != ""]
 
-commonSel = {
-    'isFiducial':(1,1),
-    #'mass':(84,97),
-}
+    nRun = len(runs)
+    for iRun, run in enumerate(runs):
+        print "@@ Analyzing run %d (%d/%d)..." % (run, iRun+1, nRun),
+        fName = "data/efficiency/run%d.txt" % run
+        if os.path.exists(fName):
+            print " already exists. skip."
+            continue
 
-print "@@ Extracting runs..."
-hRuns = hSel.Project1D("run", commonSel)
-#runs = [i+2 for i in range(hRuns.GetNbinsX()) if hRuns.GetBinContent(i+2) != 0]
-runs = [hRuns.GetXaxis().GetBinLowEdge(i+1) for i in range(hRuns.GetNbinsX()) if hRuns.GetBinContent(i+1) != 0]
-print "@@ Extracting roll names..."
-hRolls = hSel.Project1D("rollName", {}, copyAxisLabel=True)
-axisRolls = hRolls.GetXaxis()
-rollNames = [axisRolls.GetBinLabel(i) for i in range(hRolls.GetNbinsX()+2) if axisRolls.GetBinLabel(i) != ""]
+        with open(fName, "w") as fout:
+            print>>fout, "#RollName Denominator Numerator"
 
-nRun = len(runs)
-for iRun, run in enumerate(runs):
-    print "@@ Analyzing run %d (%d/%d)..." % (run, iRun+1, nRun),
-    fName = "hists/run%d.txt" % run
-    if os.path.exists(fName):
-        print " already exists. skip."
-        continue
+            subranges = commonSel.copy()
+            hDen = hSel.Project1D("rollName", subranges, suffix="_Den")
+            subranges.update({'isMatched':(1,1)})
+            hNum = hSel.Project1D("rollName", subranges, suffix="_Num")
 
-    with open(fName, "w") as fout:
-        print>>fout, "#RollName Denominator Numerator"
+            for i, name in enumerate(rollNames):
+                #### NOTE: There is a shift in the bin labels. Take rollNames with 1 bin shift
+                den = hDen.GetBinContent(i+2)
+                num = hNum.GetBinContent(i+2)
+                print>>fout, name, den, num
 
-        subranges = commonSel.copy()
-        hDen = hSel.Project1D("rollName", subranges, suffix="_Den")
-        subranges.update({'isMatched':(1,1)})
-        hNum = hSel.Project1D("rollName", subranges, suffix="_Num")
+            hDen.Delete()
+            hNum.Delete()
 
-        for i, name in enumerate(rollNames):
-            #### NOTE: There is a shift in the bin labels. Take rollNames with 1 bin shift
-            den = hDen.GetBinContent(i+2)
-            num = hNum.GetBinContent(i+2)
-            print>>fout, name, den, num
+        print ""
 
-        hDen.Delete()
-        hNum.Delete()
+    print "@@ Done."
 
-    print ""
+if __name__ == '__main__':
+    commonSel = {
+        'isFiducial':(1,1),
+        #'mass':(84,97),
+    }
 
-print "@@ Done."
+    if not os.path.exists("data/efficiency"): os.makedirs("data/efficiency")
+    project(sys.argv[1], commonSel)
