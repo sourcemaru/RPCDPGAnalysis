69f8cb0cdba0fc4c1889aea593f3255c61a3e6b4
Junghwan John Goh <jhgoh@cern.ch>
2018-09-28 15:37:07 +0900

    merge statistics if txt file already exists

--- SegmentAndTrackOnRPC/test/project_efficiency.py
+++ SegmentAndTrackOnRPC/test/project_efficiency.py
@@ -5,12 +5,13 @@ if "CMSSW_BASE" in os.environ:
     sys.path.append("%s/src/RPCDPGAnalysis/SegmentAndTrackOnRPC/python" % os.environ["CMSSW_BASE"])
 from ROOT import gStyle, TFile
 from ProjectTHnSparse import THnSparseSelector
+from collections import OrderedDict
 
-def project(fName, commonSel):
+def project(fName0, commonSel):
     gStyle.SetOptStat(0)
 
     print "@@ Opening root file..."
-    f = TFile(fName)
+    f = TFile(fName0)
     print "@@ Loading RPC TnP histogram..."
     hInfo = f.Get("rpcExt/hInfo")
     print "@@ Initializing THnSparseSelector..."
@@ -29,26 +30,38 @@ def project(fName, commonSel):
     for iRun, run in enumerate(runs):
         print "@@ Analyzing run %d (%d/%d)..." % (run, iRun+1, nRun),
         fName = "data/efficiency/run%d.txt" % run
+        effTable = OrderedDict()
         if os.path.exists(fName):
-            print " already exists. skip."
-            continue
+            for line in open(fName).readlines():
+                line = line.strip()
+                if len(line) == 0 or line[0] == '#': continue
+
+                name, den, num = line.split()
+                den, num = float(den), float(num)
+                effTable[name] = [den, num]
+
+        subranges = commonSel.copy()
+        subranges.update({'run':[run, run]})
+        hDen = hSel.Project1D("rollName", subranges, suffix="_Den")
+        subranges.update({'isMatched':(1,1)})
+        hNum = hSel.Project1D("rollName", subranges, suffix="_Num")
+
+        for i, name in enumerate(rollNames):
+            den = hDen.GetBinContent(i+1)
+            num = hNum.GetBinContent(i+1)
+            if name not in effTable:
+                effTable[name] = [den, num]
+            else:
+                effTable[name] = [den+effTable[name][0], num+effTable[name][1]]
+
+        hDen.Delete()
+        hNum.Delete()
 
         with open(fName, "w") as fout:
             print>>fout, "#RollName Denominator Numerator"
 
-            subranges = commonSel.copy()
-            subranges.update({'run':[run, run]})
-            hDen = hSel.Project1D("rollName", subranges, suffix="_Den")
-            subranges.update({'isMatched':(1,1)})
-            hNum = hSel.Project1D("rollName", subranges, suffix="_Num")
-
-            for i, name in enumerate(rollNames):
-                den = hDen.GetBinContent(i+1)
-                num = hNum.GetBinContent(i+1)
+            for name, [den, num] in effTable.iteritems():
                 print>>fout, name, den, num
-
-            hDen.Delete()
-            hNum.Delete()
 
         print ""
 
