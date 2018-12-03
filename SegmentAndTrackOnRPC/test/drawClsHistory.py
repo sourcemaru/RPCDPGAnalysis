e2c8d4c2aa91b2d239a9098b3398afcb1ac83417
Junghwan John Goh <jhgoh@cern.ch>
2018-12-04 05:19:41 +0900

    use glob, add run number on the x axis

--- SegmentAndTrackOnRPC/test/drawClsHistory.py
+++ SegmentAndTrackOnRPC/test/drawClsHistory.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-effMin, effMax = 80, 101
-method = "avgPerRoll"
-#method = "avgPerCount"
+clsMin, clsMax = 1, 3
 
 import sys, os
 from ROOT import *
@@ -23,69 +21,40 @@ gStyle.SetTitleSize(0.06, "X");
 gStyle.SetTitleSize(0.06, "Y");
 
 runs = []
-avgEffsB, avgEffsE, avgEffsRE4 = [], [], []
+avgClssB, avgClssE, avgClssRE4 = [], [], []
 
-for fName in glob("data/efficiency/Run*/run*.txt"):
+for fName in glob("data/cls/Run*/run*.txt"):
     if not os.path.exists(fName): continue
     run = int(os.path.basename(fName).split('.')[0][3:])
     runs.append(run)
 
-    if method == "avgPerRoll":
-        effsB, effsE, effsRE4 = [], [], []
-        denB, denE, denRE4 = 0., 0., 0.
-        for l in open(fName).readlines():
-            l = l.strip()
-            if len(l) == 0 or l[0] == '#': continue
+    clssB, clssE, clssRE4 = [], [], []
+    denB, denE, denRE4 = 0., 0., 0.
+    for l in open(fName).readlines():
+        l = l.strip()
+        if len(l) == 0 or l[0] == '#': continue
 
-            name, den, num = l.split()
-            den, num = float(den), float(num)
-            eff = -1
-            if den != 0: eff = num/den*100
+        name, den, mean, err2 = l.split()
+        den, mean, err2 = float(den), float(mean), float(err2)
 
-            #if eff > 70 and den > 10:
-            if eff > 0:
-                if name.startswith("W"):
-                    effsB.append(eff)
-                    denB += den
-                elif name.startswith("RE+4") or name.startswith("RE-4"):
-                    effsRE4.append(eff)
-                    denRE4 += den
-                else:
-                    effsE.append(eff)
-                    denE += den
-
-        if denB == 0: avgEffsB.append(-1)
-        else: avgEffsB.append(sum(effsB)/len(effsB))
-        if denE  == 0: avgEffsE.append(-1)
-        else: avgEffsE.append(sum(effsE)/len(effsE))
-        if denRE4 == 0: avgEffsRE4.append(-1)
-        else: avgEffsRE4.append(sum(effsRE4)/len(effsRE4))
-    elif method == "avgPerCount":
-        numB, numE, numRE4 = 0., 0., 0.
-        denB, denE, denRE4 = 0., 0., 0.
-        for l in open(fName).readlines():
-            l = l.strip()
-            if len(l) == 0 or l[0] == '#': continue
-
-            name, den, num = l.split()
-            den, num = float(den), float(num)
-
+        #if cls > 70 and den > 10:
+        if den > 0:
             if name.startswith("W"):
-                numB += num
+                clssB.append(mean)
                 denB += den
             elif name.startswith("RE+4") or name.startswith("RE-4"):
-                numRE4 += num
+                clssRE4.append(mean)
                 denRE4 += den
             else:
-                numE += num
+                clssE.append(mean)
                 denE += den
 
-        if denB < 100: avgEffsB.append(-1)
-        else: avgEffsB.append(100*numB/denB)
-        if denE < 100: avgEffsE.append(-1)
-        else: avgEffsE.append(100*numE/denE)
-        if denRE4 < 100: avgEffsRE4.append(-1)
-        else: avgEffsRE4.append(100*numRE4/denRE4)
+    if denB == 0: avgClssB.append(-1)
+    else: avgClssB.append(sum(clssB)/len(clssB))
+    if denE  == 0: avgClssE.append(-1)
+    else: avgClssE.append(sum(clssE)/len(clssE))
+    if denRE4 == 0: avgClssRE4.append(-1)
+    else: avgClssRE4.append(sum(clssRE4)/len(clssRE4))
 
 nRuns = len(runs)
 gapLS, gapTS = int(nRuns*0.1), int(nRuns*0.02)
@@ -99,9 +68,9 @@ periodsTS = {
 gapTexts = []
 
 nPoints = nRuns+(len(periodsLS)+1)*gapLS+len(periodsTS)*gapTS
-hFrame = TH1F("hFrame", "hFrame;Run number;Efficiency [%]", nPoints, 1, nPoints+1)
-hFrame.SetMinimum(effMin)
-hFrame.SetMaximum(effMax)
+hFrame = TH1F("hFrame", "hFrame;Run number;Cluster size", nPoints, 1, nPoints+1)
+hFrame.SetMinimum(clsMin)
+hFrame.SetMaximum(clsMax)
 hFrame.GetYaxis().SetNdivisions(505)
 hFrame.GetYaxis().SetTitleOffset(0.6)
 hFrame.GetXaxis().SetBinLabel(1, "")
@@ -121,20 +90,20 @@ hTS.SetLineColor(TColor.GetColor("#EEEEEE"))
 
 grpB, grpE, grpRE4 = TGraph(), TGraph(), TGraph()
 ii = 0
-for run, avgEffB, avgEffE, avgEffRE4 in sorted(zip(runs, avgEffsB, avgEffsE, avgEffsRE4), key=lambda x: x[0]):
+for run, avgClsB, avgClsE, avgClsRE4 in sorted(zip(runs, avgClssB, avgClssE, avgClssRE4), key=lambda x: x[0]):
     ii += 1
     if run in periodsLS:
         for i in range(ii, ii+gapLS): hLS.Fill(i, 100)
         hFrame.GetXaxis().SetBinLabel(ii+gapLS, "%d" % run)
 
-        #t = TText(ii+gapLS/2, effMin+(effMax-effMin)*.1, "YETS")
+        #t = TText(ii+gapLS/2, clsMin+(clsMax-clsMin)*.1, "YETS")
         #t.SetTextAlign(12)
         #t.SetTextAngle(90)
         #t.SetTextSize(0.05)
         #gapTexts.append(t)
 
-        #t = TText(ii+gapLS*1.05, effMin+(effMax-effMin)*.02, periodsLS[run])
-        t = TPaveText(ii+gapLS*1.05, effMin, ii+gapLS*2.25, effMin+(effMax-effMin)*.09, "")
+        #t = TText(ii+gapLS*1.05, clsMin+(clsMax-clsMin)*.02, periodsLS[run])
+        t = TPaveText(ii+gapLS*1.05, clsMin, ii+gapLS*2.25, clsMin+(clsMax-clsMin)*.09, "")
         t.SetFillColor(kWhite)
         t.SetBorderSize(0)
         t.SetFillStyle(1001)
@@ -148,25 +117,25 @@ for run, avgEffB, avgEffE, avgEffRE4 in sorted(zip(runs, avgEffsB, avgEffsE, avg
         for i in range(ii, ii+gapTS): hTS.Fill(i, 100)
         hFrame.GetXaxis().SetBinLabel(ii+gapTS, "%d" % run)
 
-        t = TText(ii+gapTS/2, effMin+(effMax-effMin)*.4, periodsTS[run])
+        t = TText(ii+gapTS/2, clsMin+(clsMax-clsMin)*.35, periodsTS[run])
         t.SetTextAlign(12)
         t.SetTextAngle(90)
         t.SetTextSize(0.025)
         gapTexts.append(t)
         ii += gapTS
 
-    if avgEffB > 0: grpB.SetPoint(grpB.GetN(), ii, avgEffB)
-    if avgEffE > 0: grpE.SetPoint(grpE.GetN(), ii, avgEffE)
-    if avgEffRE4 > 0: grpRE4.SetPoint(grpRE4.GetN(), ii, avgEffRE4)
+    if avgClsB > 0: grpB.SetPoint(grpB.GetN(), ii, avgClsB)
+    if avgClsE > 0: grpE.SetPoint(grpE.GetN(), ii, avgClsE)
+    if avgClsRE4 > 0: grpRE4.SetPoint(grpRE4.GetN(), ii, avgClsRE4)
 
 for i in range(ii+1, nPoints+1): hLS.Fill(i, 100)
 #gapTexts[0].SetText(gapTexts[0].GetX(), gapTexts[0].GetY(), "LS1")
-t = TText(gapLS/2, effMin+(effMax-effMin)*.4, "LS1")
+t = TText(gapLS/2, clsMin+(clsMax-clsMin)*.35, "LS1")
 t.SetTextAlign(12)
 t.SetTextAngle(90)
 t.SetTextSize(0.05)
 gapTexts.append(t)
-t = TText(ii+gapLS/2, effMin+(effMax-effMin)*.4, "LS2")
+t = TText(ii+gapLS/2, clsMin+(clsMax-clsMin)*.35, "LS2")
 t.SetTextAlign(12)
 t.SetTextAngle(90)
 t.SetTextSize(0.05)
@@ -188,7 +157,7 @@ grpB.SetLineColor(kRed)
 grpE.SetLineColor(kBlue)
 grpRE4.SetLineColor(kGreen+1)
 
-c = TCanvas("cEffHistory", "cEffHistory", 1000, 450)
+c = TCanvas("cClsHistory", "cClsHistory", 1000, 450)
 c.SetLeftMargin(0.08)
 c.SetRightMargin(0.03)
 c.SetTopMargin(0.1)
