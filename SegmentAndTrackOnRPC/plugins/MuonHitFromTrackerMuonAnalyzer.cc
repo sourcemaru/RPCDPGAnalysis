bf59334880968703bf1f4da4a6aa3bec29c2bb14
Seungjin Yang <slowmoyang@gmail.com>
2023-08-03 16:30:23 +0900

    Use multiple tokens with different transition
    Use edm::Transition::BeginRun only for RPCGeometry used in
    EDAnalyzer::beginRun
    In EDAnalyzer::analyze, use Event transition.
    
    In addtion,
    - Check whether the data product exists using Handle
    - Add MessageLogger for future debugging

--- SegmentAndTrackOnRPC/plugins/MuonHitFromTrackerMuonAnalyzer.cc
+++ SegmentAndTrackOnRPC/plugins/MuonHitFromTrackerMuonAnalyzer.cc
@@ -21,6 +21,7 @@
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "Geometry/Records/interface/MuonGeometryRecord.h"
@@ -58,9 +59,10 @@ private:
   const edm::EDGetTokenT<reco::MuonCollection> muonToken_;
   const edm::EDGetTokenT<double> tpMassToken_;
 
-  edm::ESGetToken<DTGeometry, MuonGeometryRecord> dtGeomToken_;
-  edm::ESGetToken<CSCGeometry, MuonGeometryRecord> cscGeomToken_;
-  edm::ESGetToken<RPCGeometry, MuonGeometryRecord> rpcGeomToken_;
+  const edm::ESGetToken<RPCGeometry, MuonGeometryRecord> rpc_geometry_token_begin_run_;
+  const edm::ESGetToken<DTGeometry, MuonGeometryRecord> dt_geometry_token_;
+  const edm::ESGetToken<CSCGeometry, MuonGeometryRecord> csc_geometry_token_;
+  const edm::ESGetToken<RPCGeometry, MuonGeometryRecord> rpc_geometry_token_;
 
   const double minMuonPt_, maxMuonAbsEta_;
   enum class ResonanceType { Z0, Jpsi, Upsilon, Inclusive } resonanceType_;
@@ -88,6 +90,10 @@ MuonHitFromTrackerMuonAnalyzer::MuonHitFromTrackerMuonAnalyzer(const edm::Parame
   //vertexToken_(consumes<reco::VertexCollection>(pset.getParameter<edm::InputTag>("vertex"))),
   muonToken_(consumes<reco::MuonCollection>(pset.getParameter<edm::InputTag>("muons"))),
   tpMassToken_(consumes<double>(pset.getParameter<edm::InputTag>("tpMass"))),
+  rpc_geometry_token_begin_run_(esConsumes<RPCGeometry, MuonGeometryRecord, edm::Transition::BeginRun>()),
+  dt_geometry_token_(esConsumes<DTGeometry, MuonGeometryRecord>()),
+  csc_geometry_token_(esConsumes<CSCGeometry, MuonGeometryRecord>()),
+  rpc_geometry_token_(esConsumes<RPCGeometry, MuonGeometryRecord>()),
   minMuonPt_(pset.getParameter<double>("minMuonPt")),
   maxMuonAbsEta_(pset.getParameter<double>("maxMuonAbsEta"))
 {
@@ -98,16 +104,12 @@ MuonHitFromTrackerMuonAnalyzer::MuonHitFromTrackerMuonAnalyzer(const edm::Parame
   else if ( s == "UPSILON" ) resonanceType_ = ResonanceType::Upsilon;
   else resonanceType_ = ResonanceType::Inclusive;
 
-  dtGeomToken_ = esConsumes<edm::Transition::BeginRun>();
-  cscGeomToken_ = esConsumes<edm::Transition::BeginRun>();
-  rpcGeomToken_ = esConsumes<edm::Transition::BeginRun>();
-
   hInfo_ = nullptr;
 }
 
 void MuonHitFromTrackerMuonAnalyzer::beginRun(const edm::Run& run, const edm::EventSetup& eventSetup)
 {
-  if ( hInfo_ ) return; 
+  if ( hInfo_ ) return;
 
   usesResource("TFileService");
   edm::Service<TFileService> fs;
@@ -191,15 +193,22 @@ void MuonHitFromTrackerMuonAnalyzer::beginRun(const edm::Run& run, const edm::Ev
   }
 
   // Set the roll names
-  const auto& rpcGeom = eventSetup.getData(rpcGeomToken_);
+  if (auto handle = eventSetup.getHandle(rpc_geometry_token_begin_run_)) {
+    const RPCGeometry* rpc_geometry = handle.product();
 
-  int i=0;
-  for ( const RPCRoll* roll : rpcGeom.rolls() ) {
-    const auto detId = roll->id();
-    const string rollName = RPCGeomServ(detId).name();
+    int i = 0;
+    for ( const RPCRoll* roll : rpc_geometry->rolls() ) {
+      const auto detId = roll->id();
+      const string rollName = RPCGeomServ(detId).name();
 
-    hInfo_->GetAxis(ROLLNAME)->SetBinLabel(++i, rollName.c_str());
+      hInfo_->GetAxis(ROLLNAME)->SetBinLabel(++i, rollName.c_str());
+    }
+
+  } else {
+    edm::LogError("MuonHitFromTrackerMuonAnalyzer") << "failed to get RPCGeometry";
+    return;
   }
+
 }
 
 void MuonHitFromTrackerMuonAnalyzer::analyze(const edm::Event& event, const edm::EventSetup& eventSetup)
@@ -210,9 +219,29 @@ void MuonHitFromTrackerMuonAnalyzer::analyze(const edm::Event& event, const edm:
   for ( int i=0; i<NVARS; ++i ) vars[i] = 0;
   vars[RUN] = event.id().run();
 
-  const auto& dtGeom = eventSetup.getData(dtGeomToken_);
-  const auto& cscGeom = eventSetup.getData(cscGeomToken_);
-  const auto& rpcGeom = eventSetup.getData(rpcGeomToken_);
+  const DTGeometry* dt_geometry = nullptr;
+  if (auto handle = eventSetup.getHandle(dt_geometry_token_)) {
+    dt_geometry = handle.product();
+  } else {
+    edm::LogError("MuonHitFromTrackerMuonAnalyzer") << "failed to get DTGeometry";
+    return;
+  }
+
+  const CSCGeometry* csc_geometry = nullptr;
+  if (auto handle = eventSetup.getHandle(csc_geometry_token_)) {
+    csc_geometry = handle.product();
+  } else {
+    edm::LogError("MuonHitFromTrackerMuonAnalyzer") << "failed to get CSCGeometry";
+    return;
+  }
+
+  const RPCGeometry* rpc_geometry = nullptr;
+  if (auto handle = eventSetup.getHandle(rpc_geometry_token_)) {
+    rpc_geometry = handle.product();
+  } else {
+    edm::LogError("MuonHitFromTrackerMuonAnalyzer") << "failed to get RPCGeometry";
+    return;
+  }
 
   edm::Handle<RPCRecHitCollection> rpcHitHandle;
   event.getByToken(rpcHitToken_, rpcHitHandle);
@@ -295,7 +324,7 @@ void MuonHitFromTrackerMuonAnalyzer::analyze(const edm::Event& event, const edm:
       bool hasNearbySgmt = false;
 
       const RPCDetId rpcId(match.id);
-      const RPCRoll* roll = rpcGeom.roll(match.id);
+      const RPCRoll* roll = rpc_geometry->roll(match.id);
       const auto& bound = roll->surface().bounds();
       const GlobalPoint gPos0 = roll->toGlobal(LocalPoint(0,0,0));
 
@@ -311,7 +340,7 @@ void MuonHitFromTrackerMuonAnalyzer::analyze(const edm::Event& event, const edm:
 
         if ( detId.subdetId() == MuonSubdetId::DT ) {
           const DTChamberId dtId(detId);
-          const auto dtChamber = dtGeom.chamber(detId);
+          const auto dtChamber = dt_geometry->chamber(detId);
           //if ( dtId.station() != rpcId.station() or dtId.wheel() != rpcId.ring() ) continue;
           if ( dtId.station() != rpcId.station() ) continue;
           const GlobalPoint gRefPos0 = dtChamber->toGlobal(LocalPoint(0,0,0));
@@ -322,7 +351,7 @@ void MuonHitFromTrackerMuonAnalyzer::analyze(const edm::Event& event, const edm:
         }
         else if ( detId.subdetId() == MuonSubdetId::CSC ) {
           const CSCDetId cscId(detId);
-          const auto cscChamber = cscGeom.chamber(cscId);
+          const auto cscChamber = csc_geometry->chamber(cscId);
           if ( cscId.zendcap()*cscId.station() != rpcId.region()*rpcId.station() ) continue;
           const GlobalPoint gRefPos0 = cscChamber->toGlobal(LocalPoint(0,0,0));
           const double dphi = std::abs(deltaPhi(gPos0.barePhi(), gRefPos0.barePhi()));
