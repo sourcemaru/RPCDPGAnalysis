1675d97244c88dcc3b38d02d97f65783b123670a
Seungjin Yang <slowmoyang@gmail.com>
2023-10-10 16:35:33 +0900

    use PoolSource
    Use PoolSource. When using EmptySource with run3_data, RPCGeometry does
    not contain RE+-4. It was not checked whether other rolls are missing.

--- ChamberGeometry/test/rpcGeometryDumper_cfg.py
+++ ChamberGeometry/test/rpcGeometryDumper_cfg.py
@@ -1,15 +1,29 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.AlCa.autoCond import autoCond
 
-process = cms.Process("RPCAnalysis")
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing('analysis')
+options.register(
+    name='autoCond',
+    default='run3_data',
+    mytype=VarParsing.varType.string,
+    info='a key of autoCond for global tag',
+)
+options.parseArguments()
+
+process = cms.Process('RPCAnalysis')
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
-process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load('Configuration.StandardSequences.GeometryDB_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('RPCDPGAnalysis.ChamberGeometry.rpcGeometryDumper_cfi')
 
-process.GlobalTag.globaltag = autoCond['run3_data']
+process.GlobalTag.globaltag = autoCond[options.autoCond]
 
-process.source = cms.Source("EmptySource")
-process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(1))
+process.source = cms.Source('PoolSource',
+    fileNames = cms.untracked.vstring(options.inputFiles)
+)
+process.maxEvents = cms.untracked.PSet(
+    input=cms.untracked.int32(1)
+)
 process.p = cms.Path(process.rpcGeometryDumper)
